a02d52d9f8897401aa92fa83edf2892acc5ad661
MattSpanish <128282064+MattSpanish@users.noreply.github.com>
2024-11-26 15:56:42 +0800

    remember me

--- signin&signout/LoginPage.php
+++ signin&signout/LoginPage.php
@@ -2,15 +2,20 @@
 session_start();
 require 'config.php'; // Adjust the path as necessary
 
+// Initialize variables to hold remembered username and password
+$rememberedUsername = isset($_COOKIE['remember_username']) ? $_COOKIE['remember_username'] : '';
+$rememberedPassword = isset($_COOKIE['remember_password']) ? $_COOKIE['remember_password'] : '';
+
 if (isset($_POST["submit"])) {
     $usernameemail = $_POST["usernameemail"];
     $password = $_POST["password"];
+    $rememberMe = isset($_POST['remember_me']); // Check if "Remember Me" is checked
 
     // Validate input
     if (!empty($usernameemail) && !empty($password)) {
         // Prepare the SQL statement
         $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? OR email = ?");
-        $stmt->bind_param("ss", $usernameemail, $usernameemail); // Bind parameters (username or email)
+        $stmt->bind_param("ss", $usernameemail, $usernameemail);
 
         // Execute the query
         $stmt->execute();
@@ -23,30 +28,35 @@ if (isset($_POST["submit"])) {
             if (password_verify($password, $row["password"])) {
                 // Set session variables
                 $_SESSION["login"] = true;
-                $_SESSION["id"] = $row["id"]; // Store user ID in session
+                $_SESSION["id"] = $row["id"];
+
+                // Handle the "Remember Me" functionality
+                if ($rememberMe) {
+                    setcookie('remember_username', $usernameemail, time() + (86400 * 30), "/"); // 30 days
+                    setcookie('remember_password', $password, time() + (86400 * 30), "/"); // 30 days
+                } else {
+                    setcookie('remember_username', '', time() - 3600, "/"); // Clear username cookie
+                    setcookie('remember_password', '', time() - 3600, "/"); // Clear password cookie
+                }
 
                 // Redirect to dashboard
                 header("Location: ../Faculty/profDASHBOARD.php");
                 exit;
             } else {
-                // Incorrect password
                 echo "<script>alert('Incorrect password. Please try again.');</script>";
             }
         } else {
-            // No user found
             echo "<script>alert('No account found with the provided username/email.');</script>";
         }
 
-        $stmt->close(); // Close the statement
+        $stmt->close();
     } else {
         echo "<script>alert('Please enter both username/email and password.');</script>";
     }
 }
 
-// Close the database connection
 $conn->close();
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -58,65 +68,62 @@ $conn->close();
     <link rel="stylesheet" href="./LoginPage.css"/>
 </head>
 <body>
-  <div class="container-fluid">
+<div class="container-fluid">
     <div class="row" style="margin-top: 50px;">
-      
-      <div class="col-lg-4 offset-lg-1"> <!-- Adjust the column size as per your requirement -->
-        
-          <!-- Back Arrow -->
-          <a href="../signin&signout/LandingPage.php">
+        <div class="col-lg-4 offset-lg-1">
+            <a href="LandingPage.php">
+            </a>
+            <p class="h2 font-weight-bold">UNLEASH EFFICIENCY EMPOWER YOUR WORKFORCE</p>
+            <p class="p- font-weight-normal align-bottom mt-4">
+                A Predictive Modeling for Optimal Workforce 
+                Allocation and Performance Rate Enhancement Website
+            </p>
+        </div>
+
+        <!-- Back Arrow -->
+        <a href="../signin&signout/LandingPage.php">
         <button class="back-arrow"><i class="fa-solid fa-arrow-left"></i></button>
       </a>
-      
-         
-        <p class="h2 font-weight-bold mt-4">UNLEASH EFFICIENCY EMPOWER YOUR WORKFORCE</p>
-        <p class="p- font-weight-normal align-bottom mt-4">
-          A Predictive Modeling for Optimal Workforce 
-          Allocation and Performance Rate Enhancement Website
-        </p>
-      </div>
 
-      <div class="col-lg-4 offset-lg-2"> <!-- Adjust the column size as per your requirement -->
+        <div class="col-lg-4 offset-lg-2">
+            <form action="LoginPage.php" method="post" autocomplete="off">
+                <!-- Username input -->
+                <div class="form-outline mb-4">
+                    <label class="usernameemail" for="form2Example1">Username or Email</label>
+                    <input type="text" name="usernameemail" required value="<?php echo htmlspecialchars($rememberedUsername); ?>" id="usernameemail" class="form-control" />
+                </div>
 
-        <form action="LoginPage.php" method="post" autocomplete="off">
-          <!-- Email input -->
-          <div data-mdb-input-init class="form-outline mb-4">
-            <label class="usernameemail" for="form2Example1">Username or Email </label>
-            <input type="text" name="usernameemail" required value="" id="usernameemail" class="form-control" />
-          </div>
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                    <label class="form-label" for="form2Example2">Password</label>
+                    <input type="password" name="password" required value="<?php echo htmlspecialchars($rememberedPassword); ?>" id="password" class="form-control" />
+                </div>
 
-          <!-- Password input -->
-          <div data-mdb-input-init class="form-outline mb-4">
-            <label class="form-label" for="form2Example2">Password</label>
-            <input type="password" name="password" required value="" id="password" class="form-control" />
-          </div>
+                <!-- Remember Me -->
+                <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember_me" id="form2Example31"
+                                <?php echo isset($_COOKIE['remember_username']) ? 'checked' : ''; ?> />
+                            <label class="form-check-label" for="form2Example31"> Remember me </label>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <a href="#!" class="text-success">Forgot password?</a>
+                    </div>
+                </div>
 
-          <!-- 2 column grid layout for inline styling -->
-          <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-              <!-- Checkbox -->
-              <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                <label class="form-check-label" for="form2Example31"> Remember me </label>
-              </div>
-            </div>
-            <div class="col">
-              <!-- Simple link -->
-              <a href="#!" class="text-success">Forgot password?</a>
-            </div>
-          </div>
-          <!-- Submit button -->
-          <button type="submit" name="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-success btn-block mb-4">Sign in</button>
+                <!-- Submit button -->
+                <button type="submit" name="submit" class="btn btn-success btn-block mb-4">Sign in</button>
 
-          <!-- Register buttons -->
-          <div class="text-center">
-            <p class="mb-0">Don't have an account? <a href="register.php" class="text-success">Register</a></p>
-          </div>
-
-        </form>
-      </div>
+                <!-- Register -->
+                <div class="text-center">
+                    <p class="mb-0">Don't have an account? <a href="register.php" class="text-success">Register</a></p>
+                </div>
+            </form>
+        </div>
     </div>
-  </div>
+</div>
 
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
